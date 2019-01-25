567821a812ddb445a5a116203798f3dbc756d8a1
Harry Zhang <harryzhang100@gmail.com>
2019-01-24 19:53:55 -0800

    small syntax change in Equals component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from 'components/Button';
 import './App.css';
 
 class App extends Component {
--- src/components/Equals.js
+++ src/components/Equals.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const Equals = () => 
 		
-	<div id = 'equals'>=</div>	
+	<div id = 'equals'>
+	  <button> = Button</button>	
+	  {/*will add onClick property to button soon*/}	
+	</div>	
 
 export default Equals;
